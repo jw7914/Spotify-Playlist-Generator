32a1e307d95b3e868f4ba0a1082931c910150b74
jw7914 <jw7914@nyu.edu>
2025-10-25 04:56:25 -0400

    removing routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 
 import IndexPage from "@/pages/index";
-import DocsPage from "@/pages/docs";
-import PricingPage from "@/pages/pricing";
-import BlogPage from "@/pages/blog";
 import PlaylistsPage from "@/pages/playlists";
 import TopArtistsPage from "@/pages/top-artists";
 import ProfilePage from "@/pages/profile";
@@ -14,9 +11,6 @@ function App() {
   return (
     <Routes>
       <Route element={<IndexPage />} path="/" />
-      <Route element={<DocsPage />} path="/docs" />
-      <Route element={<PricingPage />} path="/pricing" />
-      <Route element={<BlogPage />} path="/blog" />
       <Route element={<PlaylistsPage />} path="/playlists" />
       <Route element={<TopArtistsPage />} path="/top-artists" />
       <Route element={<ProfilePage />} path="/profile" />
