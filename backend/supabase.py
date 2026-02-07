32f6d0a6c2002ba840ef561113c1dde643ccafe8
jw7914 <jw7914@nyu.edu>
2026-02-07 02:03:37 -0500

    Add supabase client and dependencies

---
+++ backend/supabase.py
@@ -0,0 +1,10 @@
+from supabase import create_client, Client
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+supabase: Client = create_client(
+    os.environ.get("SUPABASE_URL"),
+    os.environ.get("SUPABASE_KEY")
+)
